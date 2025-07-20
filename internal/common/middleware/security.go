689d8da1667eff29cf42bb022448a3c21fb83efe
Benjamin Idewor <benjaminidewor@gmail.com>
2025-07-20 07:06:30 +0200

    Introduce CSRF and security middleware (#16)

---
+++ internal/common/middleware/security.go
@@ -0,0 +1,16 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func SecurityHeaders() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("X-Frame-Options", "DENY")
+		c.Header("X-Content-Type-Options", "nosniff")
+		c.Header("Referrer-Policy", "same-origin")
+		c.Header("X-XSS-Protection", "1; mode=block")
+
+		c.Next()
+	}
+}
